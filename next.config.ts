4939369d720117afe8fe80b08dbe8b80c9934294
ruehan <node.js_@kakao.com>
2024-12-17 19:18:19 +0900

    🐛 fix: 누락된 파일 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ["placehold.co"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
